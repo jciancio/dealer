b35f01bf15156e9ba92210c1184d197ba1ec69f0
jciancio <jciancio7@gmail.com>
2016-05-31 21:52:02 -0400

    added to card spec

--- lib/dealer/card.rb
+++ lib/dealer/card.rb
@@ -4,5 +4,24 @@ module Dealer
     def initialize(value, suite)
       @value, @suite = value, suite
     end
+
+    def to_s
+      "#{type} of #{suite}"
+    end
+
+    def type
+      case value
+      when 1
+        "Ace"
+      when 11
+        "Jack"
+      when 12
+        "Queen"
+      when 13
+        "King"
+      else
+        value
+      end
+    end
   end
 end
--- spec/units/card_spec.rb
+++ spec/units/card_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Dealer::Card do
-  subject{ described_class.new(3, 'clubs') }
+  subject { described_class.new(13, 'clubs') }
 
   context '#suite' do
     it "returns the correct suite" do
@@ -11,7 +11,19 @@ describe Dealer::Card do
 
   context '#value' do
     it "returns the correct value" do
-      expect(subject.value).to eq 3
+      expect(subject.value).to eq 13
+    end
+  end
+
+  context '#type' do
+    it 'returns the face for Ace, J, Q, or K' do
+      expect(subject.type).to eq 'King'
+    end
+  end
+
+  context '#to_s' do
+    it 'returns a string with the type and value' do
+      expect(subject.to_s).to eq "King of clubs"
     end
   end
 end
